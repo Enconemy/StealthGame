26eb03bcea2d5302f6975ec88a43db8b969d7e9a
Enconemy <frickmarcel@web.de>
2017-08-27 19:55:43 +0200

    Countdown
    Implemented countdown

---
+++ StealthGame/Assets/Scripts/Countdown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour {
+
+    public float StartTime = 180.0f;
+
+    private float countdown = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        countdown = StartTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0.0f)
+        {
+            SceneManager.LoadScene(2);
+        }
+	}
+
+    void OnGUI()
+    {
+        GUI.TextField(new Rect(0,0,50, 20), countdown.ToString());
+    }
+}
